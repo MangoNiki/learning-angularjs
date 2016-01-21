1c6c2efc2fd3bff6284618d3c8d1848b983e27b2
niki <niki@90zl.com>
2016-01-21 10:46:07 +0800

    init angularjs demos

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,16 +4,21 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
+        clean: {
+            build: {
+                src: ["dest/scripts"]
+            }
+        },
         uglify: {
             options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+                banner: '/*\n * This is AngularJS Demos\n * \n * Create Date: <%= grunt.template.today("yyyy-mm-dd") %>\n * \n * Powered By Niki \n */\n'
             },
             build_all: {
                 files: [{
                     expand: true,
-                    cwd: 'src/script',
+                    cwd: 'src/scripts',
                     src: '**/*.js',
-                    dest: 'dist/script'
+                    dest: 'dest/scripts'
                 }]
             }
 
@@ -23,12 +28,27 @@ module.exports = function (grunt) {
                 jshintrc: '.jshintrc',
                 reporter: require('jshint-stylish')
             },
-            all: ['Gruntfile.js', 'src/script/*.js', 'src/script/**/*.js']
+            all: ['Gruntfile.js', 'src/scripts/*.js', 'src/scripts/**/*.js']
+        },
+        cssmin: {
+            options: {
+                report: 'gzip'
+            },
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/styles',
+                    src: ['*.css', '**/*.css'],
+                    dest: 'dest/styles',
+                }]
+            }
         }
     });
 
     grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.registerTask("default", ['uglify', 'jshint']);
+    grunt.registerTask("default", ['clean', 'uglify', 'jshint','cssmin']);
 };
---
+++ dest/scripts/common.js
@@ -0,0 +1,8 @@
+/*
+ * This is AngularJS Demos
+ * 
+ * Create Date: 2016-01-21
+ * 
+ * Powered By Niki 
+ */
+$(function(){console.info("Hello")});
